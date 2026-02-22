88531aa34edcda879abd567837c6cdf5097a70e7
yklitnick <yitzchokyacovklitnick@gmail.com>
2026-02-22 02:20:39 -0500

    Add header, navigation, and user authentication functionality
    - Implemented header and navigation menu in index.php
    - Created classes for database connection, user signup, and login handling
    - Added signup and login forms with validation in signup.inc.php and login.inc.php
    - Established user data handling and error management in SignupContr and Signup classes

--- index.php
+++ index.php
@@ -13,6 +13,67 @@
 
 <body>
 
+    <header>
+        <nav>
+            <div>
+                <h3>YITZCHOK KLITNICK</h3>
+                <ul class="menu-main">
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="#">PRODUCTS</a></li>
+                    <li><a href="#">CURRENT SALES</a></li>
+                    <li><a href="#">MEMBER+</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul class="menu-member">
+                    <li><a href="#">SIGN UP</a></li>
+                    <li><a href="#" class="header-login-a">LOGIN</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <section class="index-intro">
+        <div class="index-intro-bg">
+            <div class="wrapper">
+                <div class="index-intro-c1">
+                    <div class="video"></div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut officia, fugit ea nemo aperiam quisquam consequatur earum animi illo obcaecati?</p>
+                </div>
+                <div class="index-intro-c2">
+                    <h2>We make<br>professional<br>gear</h2>
+                    <a href="#">FIND OUR GEAR HEAR</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="index-login">
+        <div class="wrapper">
+            <div class="index-login-signup">
+                <h4>SIGN UP</h4>
+                <p>Don't have an account yet? Sign up here!</p>
+                <form action="includes/signup.inc.php" method="post">
+                    <input type="text" name="uid" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <input type="password" name="pwdrepeat" placeholder="Repeat Password">
+                    <input type="text" name="email" placeholder="E-mail">
+                    <br>
+                    <button type="submit" name="submit">SIGN UP</button>
+                </form>
+            </div>
+            <div class="index-login-login">
+                <h4>LOGIN</h4>
+                <p>Already have an account? Login here!</p>
+                <form action="includes/login.inc.php" method="post">
+                    <input type="text" name="uid" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <br>
+                    <button type="submit" name="submit">LOGIN</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
